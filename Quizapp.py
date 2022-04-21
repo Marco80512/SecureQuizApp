f6b0e578ec25c2062272c236a5a97960bc39f37c
P1 <Marco80512>
2022-04-21 10:57:54 -0700

    css

--- Quizapp.py
+++ Quizapp.py
@@ -1,1 +1,16 @@
+from flask import Flask, url_for, render_template, request, Markup
 
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('layout.html')    
+
+
+    
+  
+  
+if __name__=="__main__":
+    app.run(debug=True)
+
+ 
