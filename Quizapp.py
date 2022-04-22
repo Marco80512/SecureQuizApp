d7296fda74e8f4a233c1d8b54db03a40baff4b59
P1 <Marco80512>
2022-04-22 09:54:28 -0700

    css

--- Quizapp.py
+++ Quizapp.py
@@ -3,8 +3,17 @@ from flask import Flask, url_for, render_template, request, Markup
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
+def render_layout():
+    return render_template('layout.html')  
+
+@app.route("/p")
+def render_Definition():
+    return render_template('page1.html')    
+
+@app.route("/pp")
 def render_main():
-    return render_template('layout.html')    
+    return render_template('page2.html')   
+        
 
 
     
